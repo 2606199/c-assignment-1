26c310da63e3ad780cb411a3ee5bcea1b896fa85
Jitendra singh(vicky) <68727023+2606199@users.noreply.github.com>
2020-07-24 19:16:45 +0530

    question 3
    question 3

---
+++ insertion.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int a[30];
+ int i,j,t,n,minpos;
+ cout<<"Enter limit of your array";
+ cin>>n;
+ cout<<"Enter elements of array";
+for(i=0;i<n;i++)
+{
+cin>>a[i];
+}
+
+ for(i=0;i<=n-2;i++)
+{
+ minpos=i;
+for(j=i;j<n;j++)
+{
+if(a[j]<a[minpos])
+{
+minpos=j;
+}
+}
+t=a[i];
+a[i]=a[minpos];
+a[minpos]=t;
+}
+cout<<"Sorted array is:";
+for(i=0;i<n;i++)
+{
+cout<<a[i]<<"\t";
+}
+}
+
+
+
+
+
+
+
